74ecd0ec4b62e1b23d097a8b6c9802fa8f94e499
Nisarga <nisargalokesh2@gmail.com>
2025-09-13 19:04:27 +0530

    Update data_RGB.py

--- Deblurring/data_RGB.py
+++ Deblurring/data_RGB.py
@@ -1,14 +1,31 @@
 import os
-from dataset_RGB import DataLoaderTrain, DataLoaderVal, DataLoaderTest
+from torch.utils.data import DataLoader
+from dataset_RGB import DeblurDataset
 
-def get_training_data(rgb_dir, img_options):
-    assert os.path.exists(rgb_dir)
-    return DataLoaderTrain(rgb_dir, img_options)
+def get_training_data(rgb_dirs, img_options):
+    # rgb_dirs can be a string or list
+    dataset = DeblurDataset(img_options, split='train')
+    return DataLoader(dataset,
+                      batch_size=img_options.BATCH_SIZE,
+                      shuffle=True,
+                      num_workers=4,
+                      pin_memory=True,
+                      drop_last=True)
 
-def get_validation_data(rgb_dir, img_options):
-    assert os.path.exists(rgb_dir)
-    return DataLoaderVal(rgb_dir, img_options)
+def get_validation_data(rgb_dirs, img_options):
+    dataset = DeblurDataset(img_options, split='val')
+    return DataLoader(dataset,
+                      batch_size=1,
+                      shuffle=False,
+                      num_workers=4,
+                      pin_memory=True,
+                      drop_last=False)
 
-def get_test_data(rgb_dir, img_options):
-    assert os.path.exists(rgb_dir)
-    return DataLoaderTest(rgb_dir, img_options)
+def get_test_data(rgb_dirs, img_options):
+    dataset = DeblurDataset(img_options, split='val')
+    return DataLoader(dataset,
+                      batch_size=1,
+                      shuffle=False,
+                      num_workers=4,
+                      pin_memory=True,
+                      drop_last=False)
